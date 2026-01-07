9fada0dc833d08119b17f3cb314f4f306aa34c6b
Terpity <97391618+Terpity@users.noreply.github.com>
2026-01-07 21:32:19 +1030

    Added some error-handling for failure to fetch Discord API; adjusted some header styles; swapped out bot image for better photo; updated package.json `tw` command to match new directory layout

---
+++ scripts/changeSubtitle.js
@@ -0,0 +1,13 @@
+const element = document.getElementById("changingSubtitle")
+
+const words = ["first", "only", "best",]
+index = 0
+
+element.addEventListener("click", () => {
+    index += 1
+    console.log(index)
+    if (index == (words.length)) {
+        index = 0
+    }
+    element.innerText = words[index]
+})
--- scripts/getDiscord.js
+++ scripts/getDiscord.js
@@ -1,6 +1,7 @@
 // Get Discord invite link using the server widget JSON API
 async function getDiscord() {
   const serverID = "1339520907688935528";
+  const elements = document.querySelectorAll('[data-discord-invite]');
   try {
     const response = await fetch(`https://discord.com/api/guilds/${serverID}/widget.json`);
     if (!response.ok) {
@@ -8,7 +9,6 @@ async function getDiscord() {
     }
 
     const data = await response.json();
-    const elements = document.querySelectorAll('[data-discord-invite]');
 
     if (elements.length > 0 && data.instant_invite) {
       elements.forEach(element => {
@@ -17,6 +17,17 @@ async function getDiscord() {
     }
   } catch (error) {
     console.error('Failed to load Discord invite.', error);
+    if (elements.length > 0) {
+      elements.forEach(element => {
+        element.addEventListener('click', (e) => {
+          e.preventDefault();
+        })
+        element.classList.remove("hover:text-fvt-orange")
+        element.classList.remove("hover:bg-orange-950")
+        element.classList.add("text-neutral-500")
+        element.setAttribute("title", "Unable to contact Discord API, check out our other socials!")
+      })
+    }
   }
 }
 getDiscord();
