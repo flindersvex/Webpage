cc6c022d90bd9bcae0408d4b43b51f8084d0d6b7
Terpity <me@terpity.com>
2026-02-17 15:48:20 +1030

    Merging without checking if it fails

---
+++ scripts/analytics.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1YlB6ub5GVSBr0uh3CkQ5cv4qC2MsSWY",
+  authDomain: "fvt-website.firebaseapp.com",
+  projectId: "fvt-website",
+  storageBucket: "fvt-website.firebasestorage.app",
+  messagingSenderId: "914827557069",
+  appId: "1:914827557069:web:e35ccc4e77ee105d989184",
+  measurementId: "G-T1HG2XD614"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+document.addEventListener("DOMContentLoaded", () => {
+    logEvent(analytics, "site_visited")
+})
+
+document.getElementById("joining-rubric-button").addEventListener("click", () => {
+    logEvent(analytics, "rubric-visited")
+})
+document.getElementById("joining-discord-button").addEventListener("click", () => {
+    logEvent(analytics, "discord-visited")
+})
+
